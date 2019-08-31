6ae9eb89dc92bb350febe4b3077c5f251e7e87fd
Nico Zevallos <gnastacast@gmail.com>
2019-08-31 13:06:06 -0500

    Added last requirements

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 start chrome --kiosk http://127.0.0.1:5000 --incognito --disable-pinch --overscroll-history-navigation=0 --disable-features=CrossSiteDocumentBlockingAlways,CrossSiteDocumentBlockingIfIsolating
 rem start chrome http://127.0.0.1:5000  --disable-features=CrossSiteDocumentBlockingAlways,CrossSiteDocumentBlockingIfIsolating
-C:\Users\gnastacast\Anaconda3\Scripts\activate.bat facenet && cd /d D:/Work/EunsuProject/facefinder/flask && python application.py
+C:\Users\%USERNAME%\Anaconda3\Scripts\activate.bat aura && cd /d C:\Users\%USERNAME%\facefinder\flask && python application.py
